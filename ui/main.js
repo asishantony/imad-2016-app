ec78d926f2e341cc70d96cb3a1cdbd16275e2a05
asishantony <asishantony@gmail.com>
2016-10-04 18:45:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,7 @@ button.onclick = function(){
             if(request.status===200){
                 var counter = request.responseText;
                 counter=counter+1;
+                alert(counter.toString());
                 var spaan=document.getElementById("cout");
                 spaan.innerHTML= counter.toString();
             }
