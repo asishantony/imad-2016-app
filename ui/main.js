4b514ba4d63edd3cc5922d328e2a8db2ac8e5151
asishantony <asishantony@gmail.com>
2016-10-04 15:42:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,14 @@ alert("are you ready");
 var element = document.getElementById("main");
 element.innerHTML="new value";
 var img = document.getElementById("madi");
+
 img.onclick = function()
 {
-    var counter=0;
-    img.style.marginLeft="100px";
-    counter=counter+1;
-    console.log(counter);
+  var interval= setInterval(moveRight,"10px");
+  
 };  
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+"px";
+}
