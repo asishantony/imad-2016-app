98c114aae255314d8190b10321adf04e0f72dddc
asishantony <asishantony@gmail.com>
2016-10-04 21:29:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,7 @@ button.onclick = function(){
   request.send(null);
 };
 //submit name
-var nameInput= document.getElementById("name");
+var nameInput= document.getElementById("main");
 var name=nameInput.value;
 var submit=document.getElementById("submit_btn");
 submit.onclick= function(){
