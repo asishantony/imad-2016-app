76dfdbee3b65f7fa0f00c92c017c177f4aa4f0ab
asishantony <asishantony@gmail.com>
2016-10-04 15:08:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 alert("are you ready");
 var element = document.getElementById("main");
 element.innerHTML="new value";
-var img = document.getElementById("madi");
+var img = document.getElementById("madi")
 img.onClick = function(){
     img.style.marginLeft = "100px";
     alert("finished");
