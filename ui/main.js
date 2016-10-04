79d3a6c46d35fd14fb72ddd2be9511a9de74233a
asishantony <asishantony@gmail.com>
2016-10-04 14:50:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,5 +4,5 @@ var element = document.getElementById("main");
 element.innerHTML="new value";
 var madi = document.getElementById("madi");
 madi.onClick = function(){
-    madi.style.marginLeft="100px";
+    madi.style.marginLeft = "100 px";
 };
