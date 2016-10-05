add08b6ff4cf68279d0c7badacc1175066781748
asishantony <asishantony@gmail.com>
2016-10-05 14:42:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/submit-name/name', function (req, res) {
   var name=req.params.name;
   names.push('name');
   //JSON
-  res.send(JSON.stringify(name));
+  res.send(JSON.stringify(names));
   
 });
 
