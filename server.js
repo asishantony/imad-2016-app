edfeacc7f9e0399dbf447d72381c4793fc117ca9
asishantony <asishantony@gmail.com>
2016-10-10 18:27:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/150X200pxl.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '150X2oopxl.jpg'));
 });
 var names=[];
 app.get('/submit-name/name', function (req, res) {
