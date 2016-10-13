0d4c818ff2e99ae9a6fefaeb0838cfbdd4caf705
asishantony <asishantony@gmail.com>
2016-10-13 12:28:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var names=[];
+app.get('/submit-name', function (req, res) {
+  //get the name from request 
+  var name=req.querry.name;
+  names.push('name');
+  //JSON
+  res.send(JSON.stringify(name));
+  
+});
 var counter=0;
 app.get('/counter', function (req, res) {
     
@@ -25,15 +34,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/150X200pxl.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '150X200pxl.jpg'));
 });
-var names=[];
-app.get('/submit-name', function (req, res) {
-  //get the name from request 
-  var name=req.querry.name;
-  names.push('name');
-  //JSON
-  res.send(JSON.stringify(name));
-  
-});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
