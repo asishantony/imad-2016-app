4c187ae339c6d4dcc81ac5a3fba2c1c7f3c31cd6
asishantony <asishantony@gmail.com>
2016-10-24 16:01:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ submit.onclick= function(){
               
              var names1=request.responseText;
              names= JSON.parse(names1);
-             var list="";
+           
       
             for(var i=0;i<names.length;i++){
               list += '<li>'+names+'</li>';
