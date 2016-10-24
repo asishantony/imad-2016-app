fa717145bb173c459d1a207205ce3a4a26827a49
asishantony <asishantony@gmail.com>
2016-10-24 16:18:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,7 +31,7 @@ submit.onclick= function(){
              var list="";
       
             for(var i=0;i<names.length;i++){
-              list += '<li>'+names+'</li>';
+              list += '<li>'+names[i]+'</li>';
               
              }
    
