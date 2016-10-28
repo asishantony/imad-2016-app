7cfb904c63ac295998e44be096358c49f4e9a85b
asishantony <asishantony@gmail.com>
2016-10-28 23:12:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,19 @@
 console.log('Loaded!');
-var button= document.getElementById("counter1");
+/*var button= document.getElementById("counter1");
 button.onclick = function(){
     var request= new XMLHttpRequest(); 
     request.onreadystatechange =function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status===200){
-            
+            var count=document.getElementById("count");
+       
             }
         }
     };
   //make request
   request.open('GET','http://asishantony.imad.hasura-app.io/counter',true);
   request.send(null);
-};
+};*/
 //submit name
 
 var submit=document.getElementById("submit_btn");
